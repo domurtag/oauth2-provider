36c6dfec0611224915286346582328386a0eb526
Dónal Murtagh <donal.murtagh@cognovi.ie>
2015-05-05 11:57:15 +0100

    upgraded to latest version of OAuth provider app to fix this https://github.com/bluesliverx/grails-spring-security-oauth2-provider/issues/87

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -167,5 +167,3 @@ grails.plugin.springsecurity.filterChain.chainMap = [
         '/securedOAuth2Resources/**': 'JOINED_FILTERS,-securityContextPersistenceFilter,-logoutFilter,-rememberMeAuthenticationFilter,-exceptionTranslationFilter',
         '/**': 'JOINED_FILTERS,-statelessSecurityContextPersistenceFilter,-oauth2ProviderFilter,-clientCredentialsTokenEndpointFilter,-oauth2ExceptionTranslationFilter'
 ]
-
-grails.plugin.springsecurity.oauthProvider.tokenServices.tokenEnhancerBeanNames = ['usernameTokenEnhancer']
